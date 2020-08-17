c7cca40467223e2fefe7204c913397efa7d96ca3
nikalosa <nikalosaberidze@gmail.com>
2020-08-17 21:17:33 +0400

    Test connection with front

--- functions/original/all/main.py
+++ functions/original/all/main.py
@@ -1,14 +1,14 @@
 import json
-from google.cloud import datastore
+# from google.cloud import datastore
 
-datastore_client = datastore.Client('speech-similarity')
+# datastore_client = datastore.Client('speech-similarity')
 
 
 def original_voice_get_all(request):
-    query = datastore_client.query(kind='OriginalVoice')
-    headers = {
-        'Content-type' : 'application/json',
-        'Access-Control-Allow-Origin' : '*',
-        'Access-Control-Allow-Headers' : 'Content-Type',
-    }
-    return json.dumps(list(query.fetch())), 200, headers
+    # query = datastore_client.query(kind='OriginalVoice')
+    # headers = {
+    #     'Content-type' : 'application/json',
+    #     'Access-Control-Allow-Origin' : '*',
+    #     'Access-Control-Allow-Headers' : 'Content-Type',
+    # }
+    return json.dumps(list({}))#, 200, headers
