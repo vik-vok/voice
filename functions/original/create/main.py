3272358458e50ca40f1827b3fe4ecced424a7d45
nikalosa <nikalosaberidze@gmail.com>
2020-08-18 01:01:49 +0400

    Implement user create function, missing error handling

--- functions/original/create/main.py
+++ functions/original/create/main.py
@@ -1,11 +1,23 @@
 from google.cloud import datastore
+from google.cloud import storage
 
+storage_client = storage.Client()
 datastore_client = datastore.Client('speech-similarity')
+RESULT_BUCKET = "original-voices"
 
 
 def original_voice_create(request):
 
+    # error handling is missing
+
     request_json = request.get_json(silent=True)
+    filename = request_json['name']
+    wav_file = request.files['audio_data']
+    bucket = storage_client.get_bucket(RESULT_BUCKET)
+    blob = bucket.blob()
+    blob.upload_from_file(wav_file)
+
+    request_json['voiceUrl'] = 'https://storage.googleapis.com/{}/{}.wav'.format(RESULT_BUCKET, filename)
 
     with datastore_client.transaction():
         incomplete_key = datastore_client.key('OriginalVoice')
@@ -13,6 +25,5 @@ def original_voice_create(request):
         user.update(request_json)
         datastore_client.put(user)
 
-    # firebase interaction for authentication
-    # register and save in database/firestore user object
-    return "User successfully registered"
+    return "Voice successfully created"
+
