7db37dad59b3aa8c48c60041ee58e94a2ee628e1
CSenshi <saba.pochkhua@gmail.com>
2020-08-22 15:04:58 +0400

    change get all intergace

--- functions/original/all/main.py
+++ functions/original/all/main.py
@@ -6,9 +6,6 @@ datastore_client = datastore.Client('speech-similarity')
 
 def original_voice_get_all(request):
     query = datastore_client.query(kind='OriginalVoice')
-    headers = {
-        'Content-type' : 'application/json',
-        'Access-Control-Allow-Origin' : '*',
-        'Access-Control-Allow-Headers' : 'Content-Type',
-    }
-    return json.dumps(list(query.fetch())), 200, headers
+    results = list(query.fetch())
+
+    return json.dumps(results)
