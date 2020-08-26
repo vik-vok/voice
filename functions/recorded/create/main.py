ccd0d09e901e3b662929b66d3532c079dfc39404
nikalosa <nikalosaberidze@gmail.com>
2020-08-26 21:50:27 +0400

    Debugging recorded voice create function

--- functions/recorded/create/main.py
+++ functions/recorded/create/main.py
@@ -9,7 +9,7 @@ RESULT_BUCKET = "recorded-voices"
 COMPARE_TOPIC = "compare-topic"
 
 storage_client = storage.Client(project_id)
-publisher = pubsub_v1.PublisherClient(project_id)
+publisher = pubsub_v1.PublisherClient()
 datastore_client = datastore.Client(project_id)
 
 
@@ -19,25 +19,25 @@ def recorded_voice_create(request):
     # filename = request_json['name']
     print(request_json)
     wav_file = request.files['audio_data']
-    # bucket = storage_client.get_bucket(RESULT_BUCKET)
-    # blob = bucket.blob()
-    # blob.upload_from_file(wav_file)
+    bucket = storage_client.get_bucket(RESULT_BUCKET)
+    blob = bucket.blob()
+    blob.upload_from_file(wav_file)
 
     request_json['voiceUrl'] = 'https://storage.googleapis.com/{}/{}.wav'.format(RESULT_BUCKET, filename)
 
     print(filename)
 
-    # with datastore_client.transaction():
-    #     incomplete_key = datastore_client.key('RecordedVoice')
-    #     user = datastore.Entity(key=incomplete_key)
-    #     user.update(request_json)
-    #     datastore_client.put(user)
+    with datastore_client.transaction():
+        incomplete_key = datastore_client.key('RecordedVoice')
+        user = datastore.Entity(key=incomplete_key)
+        user.update(request_json)
+        datastore_client.put(user)
 
-    # message = request_json
-    # message_data = json.dumps(message).encode('utf-8')
-    # topic_path = publisher.topic_path(project_id, COMPARE_TOPIC)
-    # future = publisher.publish(topic_path, data=message_data)
-    # future.result()
+    message = request_json
+    message_data = json.dumps(message).encode('utf-8')
+    topic_path = publisher.topic_path(project_id, COMPARE_TOPIC)
+    future = publisher.publish(topic_path, data=message_data)
+    future.result()
 
     # firebase interaction for authentication
     # register and save in database/firestore user object
