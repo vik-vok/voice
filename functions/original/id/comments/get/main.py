752245e4840cc25645af983d7d8ba802845b5009
nikalosa <nikalosaberidze@gmail.com>
2020-08-27 23:17:14 +0400

    Test

--- functions/original/id/comments/get/main.py
+++ functions/original/id/comments/get/main.py
@@ -11,19 +11,19 @@ def original_voice_comments(request):
     if request_json and 'voiceId' in request_json:
         voice_id = request_json['voiceId']
     elif request_args and 'voiceId' in request_args:
-        voice_id = int(request_args['voiceId'])
+        voice_id = request_args['voiceId']
     else:
         # return error apiresponse
         return ""
     print("bla")
-    comments_json = {}
-    comments_json = requests.get(COMMENTS_URL.format(voice_id))
-    print(comments_json.json())
-    # for i, comment in enumerate(comments_json):
-    #     user_id = comment['userID']
-    #     print(user_id)
-    #     user = requests.get(USERS_URL.format(user_id))
-    #     comments_json[i]['userID'] = user
-    #     print(user)
-    return "aeee"
-    # return json.dumps(comments_json)
+    # comments_json = {}
+    comments_json = requests.get(COMMENTS_URL.format(voice_id)).json()
+    # print(comments_json.json())
+    for i, comment in enumerate(comments_json):
+        user_id = comment['userID']
+        print(user_id)
+        user = requests.get(USERS_URL.format(user_id)).json()
+        comments_json[i]['userID'] = user
+        print(user)
+    # return "aeee"
+    return json.dumps(comments_json)
