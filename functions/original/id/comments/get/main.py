ece38d3115c5c1c9a9f6e5234afa07e3476c5810
nikalosa <nikalosaberidze@gmail.com>
2020-08-27 19:08:02 +0400

    Implement retrieving comments of original voice

--- functions/original/id/comments/get/main.py
+++ functions/original/id/comments/get/main.py
@@ -1,13 +1,26 @@
 import json
+import requests
+
+COMMENTS_URL = 'vikvok-anldg2io3q-ew.a.run.app/comments/original/{}'
+USERS_URL = 'vikvok-anldg2io3q-ew.a.run.app/users/{}'
 
 
 def original_voice_comments(request):
-    result = [{
-        "id": 123,
-        "comment": "avoie",
-        "voiceid": 14,
-        "userId": 12
-    }]
+    request_json = request.get_json(silent=True)
+    request_args = request.args
+    if request_json and 'voiceId' in request_json:
+        voice_id = request_json['voiceId']
+    elif request_args and 'voiceId' in request_args:
+        voice_id = int(request_args['voiceId'])
+    else:
+        # return error apiresponse
+        return ""
 
-    # convert into JSON:
-    return json.dumps(result)
+    comments_json = requests.get(COMMENTS_URL.format(voice_id))
+
+    for i, comment in enumerate(comments_json):
+        user_id = comment['userID']
+        user = requests.get(USERS_URL.format(user_id))
+        comments_json[i]['userID'] = user
+
+    return json.dumps(comments_json)
