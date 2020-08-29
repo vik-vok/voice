654235bd0e8b655b495b011fdb933ba9778cb1bc
nikalosa <nikalosaberidze@gmail.com>
2020-08-29 17:31:04 +0400

    Test

--- functions/recorded/create/main.py
+++ functions/recorded/create/main.py
@@ -38,20 +38,20 @@ def recorded_voice_create(request):
 
     print(filename)
 
-    with datastore_client.transaction():
-        incomplete_key = datastore_client.key('RecordedVoice')
-        user = datastore.Entity(key=incomplete_key)
-        user.update(voice)
-        datastore_client.put(user)
-        print(user)
-        voice['voiceId'] = user.key
-
-    message = voice
-    message_data = json.dumps(message).encode('utf-8')
-    topic_path = publisher.topic_path(project_id, COMPARE_TOPIC)
-    future = publisher.publish(topic_path, data=message_data)
-    future.result()
+    # with datastore_client.transaction():
+    #     incomplete_key = datastore_client.key('RecordedVoice')
+    #     user = datastore.Entity(key=incomplete_key)
+    #     user.update(voice)
+    #     datastore_client.put(user)
+    #     print(user)
+    #     voice['voiceId'] = user.key
+    #
+    # message = voice
+    # message_data = json.dumps(message).encode('utf-8')
+    # topic_path = publisher.topic_path(project_id, COMPARE_TOPIC)
+    # future = publisher.publish(topic_path, data=message_data)
+    # future.result()
 
     # firebase interaction for authentication
     # register and save in database/firestore user object
-    return message_data
+    return 'message_data'
